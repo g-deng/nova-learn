3f5e7df91181fdb7f96a0b0ea9e9d0fe744b7a8f
Grace Deng <graceful.deng@gmail.com>
2025-06-15 17:55:08 -0700

    add login functionality

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -7,19 +7,51 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { 
-  Tabs, 
-  TabsContent, 
-  TabsList, 
-  TabsTrigger 
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger
 } from "@/components/ui/tabs"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useState } from "react";
+import {
+  loginWithEmail,
+  loginWithGoogle,
+  registerWithEmail
+} from "@/lib/auth";
+import { useNavigate } from "react-router-dom";
+
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleEmailLogin = async () => {
+    try {
+      const userCred = await loginWithEmail(email, password);
+      console.log("Email login success:", userCred.user);
+      navigate("/stacks");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleGoogleLogin = async () => {
+    try {
+      const userCred = await loginWithGoogle();
+      console.log("Google login success:", userCred.user);
+      navigate("/stacks");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -38,6 +70,7 @@ export function LoginForm({
                   id="email"
                   type="email"
                   placeholder="m@example.com"
+                  onChange={e => setEmail(e.target.value)}
                   required
                 />
               </div>
@@ -51,13 +84,17 @@ export function LoginForm({
                     Forgot your password?
                   </a>
                 </div>
-                <Input id="password" type="password" required />
+                <Input
+                  id="password"
+                  type="password"
+                  onChange={e => setPassword(e.target.value)}
+                  required />
               </div>
               <div className="flex flex-col gap-3">
-                <Button type="submit" className="w-full">
+                <Button type="submit" onClick={handleEmailLogin} className="w-full">
                   Login
                 </Button>
-                <Button variant="outline" className="w-full">
+                <Button variant="outline" onClick={handleGoogleLogin} className="w-full">
                   Login with Google
                 </Button>
               </div>
@@ -74,6 +111,35 @@ export function SignupForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+
+  const handleEmailSignup = async() => {
+    try {
+      const userCred = await registerWithEmail(email, password);
+      console.log("Email registration success:", userCred.user);
+      console.log(`${confirmPassword} ${name}`); // TODO: remove this line
+      // TODO: handle user profile creation
+      navigate("/stacks");
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const handleGoogleSignup = async () => {
+    try {
+      const userCred = await loginWithGoogle();
+      console.log("Google signup success:", userCred.user);
+      // TODO: handle user profile creation
+      navigate("/stacks");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -92,6 +158,7 @@ export function SignupForm({
                   id="name"
                   type="text"
                   placeholder="Jane Doe"
+                  onChange={e => setName(e.target.value)}
                   required
                 />
               </div>
@@ -101,6 +168,7 @@ export function SignupForm({
                   id="email"
                   type="email"
                   placeholder="m@example.com"
+                  onChange={e => setEmail(e.target.value)}
                   required
                 />
               </div>
@@ -108,20 +176,30 @@ export function SignupForm({
                 <div className="flex items-center">
                   <Label htmlFor="password">Password</Label>
                 </div>
-                <Input id="password" type="password" required />
+                <Input 
+                  id="password" 
+                  type="password"
+                  onChange={e => setPassword(e.target.value)} 
+                  required 
+                />
               </div>
               <div className="grid gap-3">
                 <div className="flex items-center">
                   <Label htmlFor="confirm-password">Confirm Password</Label>
                 </div>
-                <Input id="confirm-password" type="password" required />
+                <Input 
+                  id="confirm-password" 
+                  type="password" 
+                  required
+                  onChange={e => setConfirmPassword(e.target.value)}
+                />
                 {/* TODO: add warning message when not matching */}
               </div>
               <div className="flex flex-col gap-3">
-                <Button type="submit" className="w-full">
+                <Button type="submit"  onClick={handleEmailSignup} className="w-full">
                   Sign Up
                 </Button>
-                <Button variant="outline" className="w-full">
+                <Button variant="outline" onClick={handleGoogleSignup} className="w-full">
                   Sign up with Google
                 </Button>
               </div>
